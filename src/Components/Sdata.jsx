26b0fc64e9dc00f9e2dd86e49e01bf958f77e21f
Muazam Abbas <muazamabbas11@gmail.com>
2021-12-29 22:42:50 +0500

    React JS Project Netflix App Part #1

---
+++ src/Components/Sdata.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sdata() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Sdata
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './Components/Card';
+import Card from "./Components/Card";
 import './index.css';
-import App from "./Components/App";
 
 
 ReactDOM.render(
-   
-    <App />
+    <>
 
+    <Card imgscr="https://wallpapercave.com/wp/wp4056410.jpg" title="A Netflix Orignal Seeries" sname="DARK" link="https://www.netflix.com/in/title/80990668?source=35" />
+
+    <Card imgscr="https://www.heavenofhorror.com/wp-content/uploads/2020/04/Extracurricular-netflix-review.jpg" title="A Netflix Orignal Seeries" sname="Extra Curricular" link="https://www.netflix.com/pk/title/80990668?source=35" />
+
+    <Card imgscr="https://wallpapercave.com/wp/wp1917154.jpg" title="A Netflix Orignal Seeries" sname="Strange Things" link="https://www.netflix.com/pk/title/80057281" />
+
+    </>
     , document.getElementById("root")
 );
