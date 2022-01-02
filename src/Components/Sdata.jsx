4c4728f2e8466e147f5563a4b9dd18e85d0834e6
Muazam Abbas <muazamabbas11@gmail.com>
2022-01-02 19:01:21 +0500

    Completing ReactJS Netflix App

--- src/Components/Sdata.jsx
+++ src/Components/Sdata.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+const Sdata = [
+    {
+        sname: "DARK",
+        imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "A Netflix Orignal Seeries",
+        link: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    
+    {
+        sname: "Extra Curricular",
+        imgscr: "https://www.heavenofhorror.com/wp-content/uploads/2020/04/Extracurricular-netflix-review.jpg",
+        title: "A Netflix Orignal Seeries",
+        link: "https://www.netflix.com/pk/title/80990668?source=35",
+    },
+    
+    {
+        sname: "Strange Things",
+        imgscr: "https://wallpapercave.com/wp/wp1917154.jpg",
+        title: "A Netflix Orignal Seeries",
+        link: "https://www.netflix.com/pk/title/80057281",
+    },
+    {
+        sname: "The Vampire Diaries",
+        imgscr: "https://images.justwatch.com/poster/8589251/s592",
+        title: "A Amazone Orignal Seeries",
+        link: "https://www.netflix.com/pk/title/80990668?source=35",
+    },
+    {
+        sname: "My First-2 Love",
+        imgscr: "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+        title: "Netflix orginal Series",
+        link: "https://www.netflix.com/pk/title/81026700?source=35"
+    },
+    
+];
 
-function Sdata() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default Sdata
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './Components/Card';
+import "./Components/Card";
 import Card from "./Components/Card";
-import './index.css';
+import "./index.css";
+import Sdata from "./Components/Sdata";
 
 
 ReactDOM.render(
-    <>
-
-    <Card imgscr="https://wallpapercave.com/wp/wp4056410.jpg" title="A Netflix Orignal Seeries" sname="DARK" link="https://www.netflix.com/in/title/80990668?source=35" />
-
-    <Card imgscr="https://www.heavenofhorror.com/wp-content/uploads/2020/04/Extracurricular-netflix-review.jpg" title="A Netflix Orignal Seeries" sname="Extra Curricular" link="https://www.netflix.com/pk/title/80990668?source=35" />
-
-    <Card imgscr="https://wallpapercave.com/wp/wp1917154.jpg" title="A Netflix Orignal Seeries" sname="Strange Things" link="https://www.netflix.com/pk/title/80057281" />
-
-    </>
-    , document.getElementById("root")
+  <>
+    <h1 className="heading_style">List of top 5 Netflix Series in 2020</h1>
+    {Sdata.map((val) => {
+        return (
+            <Card
+              imgscr={val.imgscr}
+              title={val.title}
+              sname={val.sname}
+              link={val.link}
+            />
+          );
+    })};
+  </>,
+  document.getElementById("root")
 );
